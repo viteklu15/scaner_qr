60e457767e2cc374f52005ca71e28a885e7ce6d0
viteklu15 <viteklu15@gmail.com>
2025-09-03 14:50:39 +0300

    изменил версию fliter

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.scaner_qr"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"         // ← поставить требуемую версию
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
