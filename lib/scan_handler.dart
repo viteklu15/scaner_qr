00754928735c0e818bd0d67745bf9090e447aaec
viteklu15 <viteklu15@gmail.com>
2025-09-09 13:23:32 +0300

    вроде работает

--- lib/scan_handler.dart
+++ lib/scan_handler.dart
@@ -1,12 +1,15 @@
-import 'package:flutter/services.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+final _player = AudioPlayer();
+
 Future<void> handleScan(String code) async {
-  await SystemSound.play(SystemSoundType.alert);
-  final Uri uri = Uri.parse('https://ya.ru/$code');
-  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+  // Проигрываем beep.mp3 из assets
+  _player.play(AssetSource('sounds/beep.mp3'));
+
+  final Uri uri = Uri.parse('http://192.168.0.127:5002/edit/$code');
+  if (!await launchUrl(uri, mode: LaunchMode.inAppWebView)) {
     // ignore: avoid_print
     print('Could not launch $uri');
   }
 }
-
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import mobile_scanner
+import path_provider_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
